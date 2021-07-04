a5c10d9d6caff2d9475181cd87ef0a37920ecffb
Vitalii Godlevskyi <68828801+VGodlevskyi@users.noreply.github.com>
2021-07-04 13:49:04 +0300

    Update Main.java
    123

--- services/api/src/main/java/io/swec/services/api/Main.java
+++ services/api/src/main/java/io/swec/services/api/Main.java
@@ -3,7 +3,7 @@ package io.swec.services.api;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-
+cc
 import java.io.IOException;
 import java.net.URI;
 
